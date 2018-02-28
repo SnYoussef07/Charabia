112ff33c6fe081d67ec076f86f40d68fd71baf29
41385@etu.he2b.be <41385@etu.he2b.be>
2018-02-28 17:17:58 +0100

    Ajout de CharabiaGame

---
+++ src/g41385/charabia/model/CharabiaGame.java
@@ -0,0 +1,39 @@
+package g41385.charabia.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author 41385
+ */
+public abstract class CharabiaGame implements Charabia{ //enlever abstract!!!
+
+    /*@Override
+    public Player joinGame(String playerName) {
+    }
+    @Override
+    public List<Player> getPlayers() {
+    }
+    @Override
+    public List<Tile> getTiles() {
+    }
+    @Override
+    public void play(Player player, String word) {
+    }
+    @Override
+    public List<Player> getRoundWinners() {
+    }
+    @Override
+    public List<Player> getWinners() {
+    }
+    @Override
+    public void nextRound() {
+    }
+    @Override
+    public boolean isRoundOver() {
+    }
+    @Override
+    public boolean isGameOver() {
+    }*/
+    
+}
