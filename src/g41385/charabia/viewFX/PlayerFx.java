05aba2dc14c2b1138ffb79e7cc2670b4553d3f56
41385@etu.he2b.be <41385@etu.he2b.be>
2018-03-22 20:56:46 +0100

    Modification visuale des joueur

--- src/g41385/charabia/viewFX/PlayerFx.java
+++ src/g41385/charabia/viewFX/PlayerFx.java
@@ -14,26 +14,61 @@ import javafx.scene.layout.VBox;
 public class PlayerFx extends VBox {
 
     private Charabia charabia;
-    private HBox hboxPlayerOne;
-    private HBox hboxPlayerTwo;
+    private VBox hboxPlayerOne;
+    private VBox hboxPlayerTwo;
+    private Label nameOne;
+    private Label nameTwo;
+    private Label scoreOne;
+    private Label scoreTwo;
 
     public PlayerFx(Charabia game) {
         charabia = game;
-        hboxPlayerOne = new HBox();
-        hboxPlayerTwo = new HBox();
+        hboxPlayerOne = new VBox();
+        hboxPlayerTwo = new VBox();
+        hboxPlayerOne.setStyle("-fx-background-color: #639bb6;\n"
+                + "-fx-border-radius: 7 7 7 7;\n"
+                + "-fx-background-radius: 7 7 7 7;");
+        hboxPlayerTwo.setStyle("-fx-background-color: #76b99c;\n"
+                + "-fx-border-radius: 7 7 7 7;\n"
+                + "-fx-background-radius: 7 7 7 7;");
+        nameOne = new Label();
+        nameTwo = new Label();
+        scoreOne = new Label();
+        scoreTwo = new Label();
+        setStyleLabel();
+
         myPlayers();
+    }
+
+    private void setStyleLabel() {
+        nameOne.setStyle("-fx-font-size: 20px;\n"
+                + "-fx-font-weight: bold;\n"
+                + "-fx-text-fill: #000000;\n"
+                + "-fx-effect: dropshadow( gaussian , rgba(255,255,255,0.5) , 0,0,0,1 );");
+        nameTwo.setStyle("-fx-font-size: 20px;\n"
+                + "-fx-font-weight: bold;\n"
+                + "-fx-text-fill: #000000;\n"
+                + "-fx-effect: dropshadow( gaussian , rgba(255,255,255,0.5) , 0,0,0,1 );");
+        scoreOne.setStyle("-fx-font-size: 20px;\n"
+                + "-fx-font-weight: bold;\n"
+                + "-fx-text-fill: #000000;\n"
+                + "-fx-effect: dropshadow( gaussian , rgba(255,255,255,0.5) , 0,0,0,1 );");
+        scoreTwo.setStyle("-fx-font-size: 20px;\n"
+                + "-fx-font-weight: bold;\n"
+                + "-fx-text-fill: #000000;\n"
+                + "-fx-effect: dropshadow( gaussian , rgba(255,255,255,0.5) , 0,0,0,1 );");
     }
 
     private void myPlayers() {
         this.getChildren().clear();
         hboxPlayerOne.getChildren().clear();
         hboxPlayerTwo.getChildren().clear();
-        
-        Label nameOne = new Label("name = " + charabia.getPlayers().get(0).getName() + " || ");
-        Label nameTwo = new Label("name = " + charabia.getPlayers().get(1).getName() + " || ");
-        Label scoreOne = new Label("Score = " + charabia.getPlayers().get(0).getScore());
-        Label scoreTwo = new Label("Score = " + charabia.getPlayers().get(1).getScore());
-        
+
+        nameOne.setText("Nom : " + charabia.getPlayers().get(0).getName());
+        nameTwo.setText("Nom : " + charabia.getPlayers().get(1).getName());
+        scoreOne.setText("Score : " + charabia.getPlayers().get(0).getScore());
+        scoreTwo.setText("Score :" + charabia.getPlayers().get(1).getScore());
+
         hboxPlayerOne.getChildren().addAll(nameOne, scoreOne);
         hboxPlayerTwo.getChildren().addAll(nameTwo, scoreTwo);
         this.getChildren().addAll(hboxPlayerOne, hboxPlayerTwo);
@@ -41,8 +76,9 @@ public class PlayerFx extends VBox {
         hboxPlayerTwo.setAlignment(Pos.CENTER_RIGHT);
         this.setAlignment(Pos.CENTER);
         this.setPadding(new Insets(0, 10, 0, 00));
+        this.setSpacing(20);
     }
-    
+
     public void refreshPlayers() {
         myPlayers();
     }
